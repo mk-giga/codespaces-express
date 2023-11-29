806191b7f13a917c5a802187065c8182d2333cae
MK Giga <107154937+mk-giga@users.noreply.github.com>
2023-11-29 23:52:48 +0000

    boilerplate

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
 
+const express = require('express');
+const app = express();
+
+const CONFIG = {
+  PORT: 3000,
+  IP: "localhost"
+}
+
+// when we get HTTP request on /
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${CONFIG.PORT}`);
+});
---
+++ public/main.js
@@ -0,0 +1,11 @@
+const getElementById = document.getElementById;
+const getElementByClassName = document.getElementsByClassName;
+
+const canvas = getElementById("canvas");
+const ctx = canvas.getContext('2d');
+
+function main() {
+
+}
+
+main();
